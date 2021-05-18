d4fd3612c69e5358f311157790724a372ae8b985
Anwesh43 <anweshthecool0@gmail.com>
2021-05-18 14:30:49 +0530

    adding PumpSquare clas

--- pumpsquareview/src/main/java/com/example/pumpsquareview/PumpSquareView.kt
+++ pumpsquareview/src/main/java/com/example/pumpsquareview/PumpSquareView.kt
@@ -166,4 +166,27 @@ class PumpSquareView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class PumpSquare(var i : Int) {
+
+        private var curr : PSNode = PSNode(i)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun udpate(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
+
 }
