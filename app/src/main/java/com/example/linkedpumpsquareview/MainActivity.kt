96a652783e76f4b850a380bdbe0223cf64d3e104
Anwesh43 <anweshthecool0@gmail.com>
2021-05-18 14:43:16 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedpumpsquareview/MainActivity.kt
+++ app/src/main/java/com/example/linkedpumpsquareview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.linkedpumpsquareview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.pumpsquareview.PumpSquareView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PumpSquareView.create(this)
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
